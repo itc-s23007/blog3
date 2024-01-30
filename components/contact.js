f5837633905ec87dd99ba3b4f95d9dcc1a0ba346
s23007 <s23007@std.it-college.ac.jp>
2024-01-30 07:39:21 +0000

    5.10 アイコンのサイズ（フォントサイズ)をpropsで指定できるようにする

--- components/contact.js
+++ components/contact.js
@@ -4,7 +4,7 @@ import styles from 'styles/contact.module.css'
 const Contact = () => (
   <div className={styles.stack}>
     <h3 className={styles.heading}>Contact</h3>
-    <Social />
+    <Social iconSize='30px' />
     <address>cube@web.mail.address</address>
   </div>
 )
