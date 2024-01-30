983daf3bfa995fe32c0939a3c320ca981dd80f2c
s23007 <s23007@std.it-college.ac.jp>
2024-01-30 07:35:55 +0000

    5.9 アイコンを使ってソーシャルリンクメニューを作成する

--- components/contact.js
+++ components/contact.js
@@ -1,8 +1,10 @@
+import Social from 'components/social'
 import styles from 'styles/contact.module.css'
 
 const Contact = () => (
   <div className={styles.stack}>
     <h3 className={styles.heading}>Contact</h3>
+    <Social />
     <address>cube@web.mail.address</address>
   </div>
 )
