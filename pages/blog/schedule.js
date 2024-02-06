56664b8caaab8d00d627a0e3365c2ef0c640b461
s23007 <s23007@std.it-college.ac.jp>
2024-02-06 07:39:56 +0000

    9.1 アイキャッチ画像eyecatchの代替とブラー画像を用意する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,7 +12,10 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getImageBuffer } from 'lib/getImageBuffer'
+import { getPlaiceholder } from 'plaiceholder'
 
+import { eyecatchLocal } from 'lib/constants'
 const Schedule = ({
   title,
   publish,
@@ -41,6 +44,8 @@ const Schedule = ({
           height={eyecatch.height}
           sizes='(min-width: 1152px) 1152px, 100vw'
           priority
+          placeholder='blur'
+          blurDataURL={eyecatch.blurDataURL}
         />
       </figure>
 
@@ -59,20 +64,25 @@ const Schedule = ({
 )
 
 const getStaticProps = async () => {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
 
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const imageBuffer = await getImageBuffer(eyecatch.url)
+  const { base64 } = await getPlaiceholder(imageBuffer)
+  eyecatch.blurDataURL = base64
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
-      description
+      description: description
     }
   }
 }
