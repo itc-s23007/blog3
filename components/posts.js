5f669b0e526f54a2496b8f949a295fc6c84808cc
s23007 <s23007@std.it-college.ac.jp>
2024-02-09 09:24:09 +0000

    9.6 記事一覧にnext/imageでアイキャッチ画像を追加する

--- components/posts.js
+++ components/posts.js
@@ -1,12 +1,25 @@
 import styles from 'styles/posts.module.css'
 import Link from 'next/link'
+import Image from 'next/legacy/image'
 
 const Post = ({ posts }) => {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }) => (
+      {posts.map(({ title, slug, eyecatch }) => (
         <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
+            <figure>
+              <Image
+                src={eyecatch.url}
+                alt=''
+                layout='fill'
+                width={eyecatch.width}
+                height={eyecatch.height}
+                sizes='(min-width: 1152px) 576px, 50vw'
+                plaiceholder='blur'
+                blurDataURL={eyecatch.blurDataURL}
+              />
+            </figure>
             <h2>{title}</h2>
           </Link>
         </article>
